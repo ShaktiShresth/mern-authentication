3a6dea2a5c41e17d1abda8d5281d639dd51b5ba0
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-05-16 15:38:30 +0545

    Changed mailtrap domain email and name

--- backend/mailtrap/mailTrap.config.js
+++ backend/mailtrap/mailTrap.config.js
@@ -9,6 +9,6 @@ export const mailtrapClient = new MailtrapClient({
 });
 
 export const sender = {
-  email: "hello@demomailtrap.co",
-  name: "Shakti Shrestha",
+  email: "hello@mernauthentication.frii.site",
+  name: "Mailtrap Test",
 };
