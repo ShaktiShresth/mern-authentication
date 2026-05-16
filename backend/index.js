f72c0ab3b5d33043e003d46b593ee0213d7962bf
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-05-16 13:19:13 +0545

    Catch-all route regex

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,8 @@ app.use("/api/auth", authRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  // Catch-all route using regex (works with all Express versions)
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
